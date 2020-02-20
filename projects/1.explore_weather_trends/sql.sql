96bafa1b21421bab601ece7fb9ecf78c2b2b72a6
Daniel Bodmer <danny.bodmer@gmail.com>
2020-02-20 19:55:13 +0000

    Update project 1 sql file

--- projects/1.explore_weather_trends/sql.sql
+++ projects/1.explore_weather_trends/sql.sql
@@ -12,16 +12,29 @@ SELECT year
    AND country = 'United Kingdom'
    AND avg_temp IS NOT NULL;
 
+--get a sample of western capital cities and most polluted cities
 SELECT year
        avg_temp,
        city,
-       country
+       country,
+       'is_capital' AS category
   FROM city_data
  WHERE (city = 'London'
    AND country = 'United Kingdom')
-   OR  city IN ('Paris', 'Kanpur', 'Shanghai');
+    OR city IN ('Paris' --more cities here
+   )
 
---Global data
+ UNION ALL
+
+SELECT year,
+       avg_temp,
+       city,
+       country,
+       'is_polluted' AS category
+  FROM city_data
+ WHERE city in ('Nagpur');
+
+--Global data -- for comparison of cities against global average
 SELECT year,
        avg_temp
   FROM global_data;

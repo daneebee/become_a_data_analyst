f623881a7ccd3833961a32437d2cf17f29346000
Daniel Bodmer <danny.bodmer@gmail.com>
2020-02-20 18:12:04 +0000

    Update project 1 sql file

--- projects/1.explore_weather_trends/sql.sql
+++ projects/1.explore_weather_trends/sql.sql
@@ -1,11 +1,27 @@
---Data for my nearest local city
-SELECT year,
-       avg_temp
+--Data for cities
+/*
+filtered on country as there is also London in Canada
+and removed records where no temp was recorded
+*/
+SELECT year
+       avg_temp,
+       city,
+       country
   FROM city_data
  WHERE city = 'London'
    AND country = 'United Kingdom'
    AND avg_temp IS NOT NULL;
 
+SELECT year
+       avg_temp,
+       city,
+       country
+  FROM city_data
+ WHERE (city = 'London'
+   AND country = 'United Kingdom')
+   OR  city IN ('Paris', 'Kanpur', 'Shanghai');
+
 --Global data
-SELECT *
+SELECT year,
+       avg_temp
   FROM global_data;
